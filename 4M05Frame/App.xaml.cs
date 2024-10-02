35cc74e8c4a7d195e6d3430fcba3c6f8641e94e4
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-10-02 08:22:50 +0200

    Dodaj pliki projektów.

---
+++ 4M05Frame/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4M05Frame
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4M05Frame/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace _4M05Frame
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
